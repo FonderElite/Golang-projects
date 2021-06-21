13e5c04f691321d28e3e974e7a13261ec119f221
FonderElite <s1170044@usls.edu.ph>
2021-06-21 08:10:07 +0000

    Create request.go

---
+++ Intermediate/request.go
@@ -0,0 +1,38 @@
+package main
+import (
+        "fmt"
+        "net/http"
+        "log"
+)
+
+type Request struct{
+url string
+wordlist string
+}
+func CheckSite(url string) *Request{
+req = Request{url:url}
+req.url = url
+resp, err := http.Get(req.url)
+if err != nil{
+log.Fatal(err)
+}else if resp.statusCode == 200{
+fmt.Printf("[%v]Success Status Code.",resp.statusCode)
+        fmt.Println(resp.Body)
+}else{
+fmt.Println("Error.")
+}
+defer resp.Body.Close()
+return &req
+}
+func main(){
+        fmt.Println("""
+     ___ ___  __      __   ___  __        ___  __  ___ 
+|__|  |   |  |__)    |__) |__  /  \ |  | |__  /__`  |  
+|  |  |   |  |       |  \ |___ \__X \__/ |___ .__/  |  
+By FonderElite | Github:https://github.com/FonderElite
+        """)
+var site string
+fmt.Println("Enter Url: ")
+fmt.Scanln(&site)
+CheckSite(site)
+}
